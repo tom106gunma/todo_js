a855b40f4ee21af211afcdcd398f46f9ba5fee9b
tom106gunma <>
2022-11-02 08:02:05 +0900

    追加をクリックするとボタンも追加されるように変更

--- src/index.js
+++ src/index.js
@@ -5,16 +5,35 @@ const onClickAdd = () => {
 
   // div
   const div = document.createElement("div");
+  div.className = "list-row";
 
   // liタグ作成
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // 完了ボタン作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  })
+
+  // 削除ボタン作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  })
+
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
 
   // 未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
