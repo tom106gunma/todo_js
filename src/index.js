6b11ad5e6515deb5f5d9fd446ba02ac1d4d9b9f2
tom106gunma <108250537+tom106gunma@users.noreply.github.com>
2022-11-01 22:26:39 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div
+  const div = document.createElement("div");
+
+  // liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
